1da8a3691942328e84946bb94efca6678573b61d
Thomas Ankcorn <thomasankcorn@gmail.com>
2022-12-01 17:38:57 +0000

    day 1 part 2

--- day1.go
+++ day1.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -18,23 +20,37 @@ func main() {
 
 	var eatinglist = string(dat)
       var max = 0
+	var list []int
 	for _, b :=range strings.Split(eatinglist, "\n\n") {
 		var total int = 0
 		for _, c := range strings.Split(b, "\n") {
 			if len(c) > 0 {
-				print(c)
 				calories, err := strconv.Atoi(c)
 				check(err)
 				total = total + calories
 			}
 			
 		}
+		list = append(list, total)
 		if total > max {
 			max = total
 		}
 	}
 
 	print("max: ", max, "\n")
+	sort.Ints(list)
+
+	var listTop3 = []int{list[len(list)-1], list[len(list)-2], list[len(list)-3]}
+	fmt.Printf("top 3 %v\n", listTop3)
+
+	var sumTop3 int
+	for _, cals := range listTop3 {
+		sumTop3 = sumTop3 + cals
+	}
+
+	print("sumTop3: ", sumTop3,  "\n")
+
+
 
 
 }
