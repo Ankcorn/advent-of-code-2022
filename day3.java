76de20478c1c60defb9c7c328dcf546fb603174a
Thomas Ankcorn <thomasankcorn@gmail.com>
2022-12-04 10:13:10 +0000

    tidy up java day

--- day3.java
+++ day3.java
@@ -2,16 +2,47 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
-public class day3p2 {
+public class day3 {
   public static void main(String[] args) {
     try {
       File myObj = new File("day3.txt");
       Scanner myReader = new Scanner(myObj);
-      int total = 0;
+      int part1 = 0;
       while (myReader.hasNextLine()) {
-        String data1 = myReader.nextLine();
-        String data2 = myReader.nextLine();
-        String data3 = myReader.nextLine();
+        String data = myReader.nextLine();
+        int pocketSize = data.length() / 2;
+        String pocket1 = data.substring(0, pocketSize);
+        String pocket2 = data.substring(pocketSize);
+        
+        char[] items = pocket2.toCharArray();
+        
+        for(int i = 0; i < items.length; i++) {
+          if(pocket1.contains(String.valueOf(items[i]))) {
+
+            char match = items[i];
+
+            int unformatedPriority = (int) match;
+            if(unformatedPriority >= 97) {
+              part1 += unformatedPriority - 96;
+            } else {
+              part1 += unformatedPriority - 38;
+            }
+            break;
+          } 
+        }
+       
+        
+
+      }
+      System.out.println("part1: " + part1);
+      myReader.close();
+
+      Scanner input2 = new Scanner(myObj);
+      int part2 = 0;
+      while (input2.hasNextLine()) {
+        String data1 = input2.nextLine();
+        String data2 = input2.nextLine();
+        String data3 = input2.nextLine();
 
         found:
         for(int y = 0; y< data1.length(); y++) {
@@ -22,11 +53,9 @@ public class day3p2 {
                                     
                                     int unformatedPriority = (int) match;
                                     if(unformatedPriority >= 97) {
-                                    System.out.printf("%s %d %n", match, unformatedPriority - 96);
-                                    total += unformatedPriority - 96;
+                                    part2 += unformatedPriority - 96;
                                     } else {
-                                    System.out.printf("%s %d %n", match, unformatedPriority - 38);
-                                    total += unformatedPriority - 38;
+                                    part2 += unformatedPriority - 38;
                                     }
                                  break found;
                               }
@@ -34,8 +63,8 @@ public class day3p2 {
                   }
             }
       }
-      System.out.println("total: " + total);
-      myReader.close();
+      System.out.println("part2: " + part2);
+      input2.close();
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
